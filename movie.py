ba911003f8b36b22f4bcad6823c46689b8417676
Kevin Mayo <kevin@mayoind.com>
2015-02-06 00:41:18 -0700

    fix title/date

--- movie.py
+++ movie.py
@@ -1,7 +1,7 @@
 class Movie():
     """ This class stores information about a movie """
 
-    def __init__(self, release_date, movie_title, movie_storyline, poster_image,trailer_youtube):
+    def __init__(self, movie_title, release_date, movie_storyline, poster_image,trailer_youtube):
         self.title = movie_title
         self.date = release_date
         self.storyline = movie_storyline
